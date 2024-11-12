062a3be7c31bcfb390806e6e15b2801805738e9e
Lang Qin <112713160+LangQIN06@users.noreply.github.com>
2024-11-11 21:09:57 -0500

    Update arduino_secrets.h

--- thesis_ITP_2024/tactile_IO_2/arduino_secrets.h
+++ thesis_ITP_2024/tactile_IO_2/arduino_secrets.h
@@ -1,6 +1,4 @@
-#define SECRET_SSID "Lang’s iPhone"  // network name
-#define SECRET_PASS "ql-575757"      // network password
-// #define SECRET_SSID "sandbox370"  // network name
-// #define SECRET_PASS "+s0a+s03!2gether?"      // network password
+#define SECRET_SSID "xxxx"  // network name
+#define SECRET_PASS "0000"      // network password
 #define SECRET_MQTT_USER "public"    // broker username
 #define SECRET_MQTT_PASS "public"    // broker password
